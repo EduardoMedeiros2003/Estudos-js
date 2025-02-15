962ed992508ed34dce13bd6b35c97f718fa358c8
EduardoMedeiros2003 <umbelino.eduardo2003@gmail.com>
2025-02-14 22:03:22 -0300

    Add files via upload

---
+++ conta.js
@@ -0,0 +1,17 @@
+//so lembrando como funciona as variaveis no js para fazer contas
+let salario = 1300;
+let conta = 963;
+salario = 2500
+const resultado = salario- conta;
+
+
+console.log(resultado);
+console.log('Ir em busca de um salario de 5987 pra mais, se possivel esse ano ganhar mais')
+console.log('Conseguir tudo isso com a programação')
+
+let novosalario = 6980
+console.log(novosalario)
+
+//acordar todo dia de 8 horas da manah ir treinar de manha e estudar umas 2 horas por dia focar muito esses dias 
+//vencer todo dia um pouco vai me levar aonde eu quero chegar so manter esse foco todo dia, vai da certo pq sou determinado 
+//vencer o fisico, o mental estudando e lendo, e a minha propria mente'não temos inimigos' 
---
+++ logica.js
@@ -0,0 +1,6 @@
+// Escreva uma função que recebe 2 números e retorne o maior deles
+
+const max = (x, y) =>  x > y ? x : y;  
+console.log(max(80, 70));
+
+//aula de logica de programação que mostra como os codigos pode ser pequenos e tem varias formas de realizar a mesmas coisas 
