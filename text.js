bd6270fb30b60a37ce62b7a840fe9e8ea92783bd
Eduardo Umbelino <umbelino.eduardo2003@gmail.com>
2025-04-28 20:39:53 -0300

    função de repetir frase
    pequena adição no text.js. função de repedir qualquer frase

--- text.js
+++ text.js
@@ -4,3 +4,10 @@ console.log('Textando o GITHUB com o projeto')
 const pessoa = 'Carlos'
 
 console.log('oi gostaria de falar com', pessoa )
+
+function repetirFrase(quantidade) {
+    for (let i = 0; i < quantidade; i++) {
+        console.log('Estude, Pratique, Erre, Repita');
+    }
+}
+repetirFrase(5);
