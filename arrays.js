82d74adcfdf9eec14019f249e294d9ca2c6ae723
Eduardo Umbelino <umbelino.eduardo2003@gmail.com>
2025-05-07 22:26:47 -0300

    Arryas
    Pequena revisão de Arryas

---
+++ arrays.js
@@ -0,0 +1,31 @@
+//Valor por referencia
+//                 0           1          2           3          4
+const nomes = ['Eduardo', 'Umbelino', 'Gnçalves', 'Tabosa', 'Medeiros'];
+const novo = [...nomes];
+//Slice fatia o array e puxa so os indicis indicados
+const novos = nomes.slice(0,-1);
+
+console.log(novos);
+
+//pop remove o elemento do final do array e pode ser uma variavel que ira remover e chmar 
+novo.pop();
+//push = Adiciona um elemento novo no final do array
+nomes.push('João');
+//unshift ira adicionar um novo elemento no inicio do array, que no exmplo esta sendo deletado e entrando na variavel
+nomes.unshift('Carlos');
+//shift vai remover o primeiro elemento do array- demostrando ele em uma variavel agora
+const removidos = nomes.shift();
+console.log(nomes, removidos);
+// o que faz em novo não reflete em nomes 
+console.log(novo);
+//qualo o tamanho do array, é um metodo por isso não usa '' - tem 3 elementros
+console.log(nomes.length);
+
+const string = 'Carlos Eduardo Tabosa Medeiros';
+//trasformando uma string em uma array com o split e mostrando como cada elemento esta sendo dividido
+const strings = string.split(' ');
+console.log(strings);
+
+//trasformando um array em uma string 
+const strng1 = nomes.join(' ');
+console.log(strng1)
